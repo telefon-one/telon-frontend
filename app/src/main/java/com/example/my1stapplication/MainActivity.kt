b0b35cd70fbaf2fce00e39d54a2305da6cfc9b07
Titan <titantrollzpb@gmail.com>
2024-12-04 20:54:42 +0700

    beta v0.01

---
+++ app/src/main/java/com/example/my1stapplication/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.my1stapplication
+
+import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.Switch
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        // Handle system bars insets
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+
+    }
+
+}
+
+
