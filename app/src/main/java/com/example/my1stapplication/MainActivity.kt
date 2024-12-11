7550e7c6e61e5b3ec4b7503df1799ebb9ab58566
WishayutC <titantrollzpb@gmail.com>
2024-12-11 22:08:50 +0700

    beta v0.02

--- app/src/main/java/com/example/my1stapplication/MainActivity.kt
+++ app/src/main/java/com/example/my1stapplication/MainActivity.kt
@@ -1,31 +1,21 @@
 package com.example.my1stapplication
 
+import android.content.Intent
 import android.os.Bundle
-import android.widget.RadioButton
-import android.widget.Switch
-import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        // Handle system bars insets
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val openSettingsButton = findViewById<Button>(R.id.open_settings_button)
+
+        openSettingsButton.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
         }
-
-
-
     }
-
 }
-
-
