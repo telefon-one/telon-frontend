d1a18b2427b72165e4580b207fdfaa92d76ec85b
Titan <titantrollzpb@gmail.com>
2024-12-18 19:03:03 +0700

    v0.03

--- app/src/main/java/com/example/my1stapplication/LogActivity.kt
+++ app/src/main/java/com/example/my1stapplication/LogActivity.kt
@@ -2,22 +2,20 @@ package com.example.my1stapplication
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
-
+class LogActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.log_activity)  // Ensure this is the correct layout file
 
-        val openSettingsImage = findViewById<ImageView>(R.id.open_settings_button)
-
-
-        openSettingsImage.setOnClickListener {
+        // Find the nav_arrow ImageView and set a click listener
+        val nav_arrow = findViewById<ImageView>(R.id.nav_arrow)
+        nav_arrow.setOnClickListener {
             val intent = Intent(this, SettingsActivity::class.java)
             startActivity(intent)
+            finish()  // Optionally close LogActivity
         }
     }
 }
--- app/src/main/java/com/example/my1stapplication/SettingsActivity.kt
+++ app/src/main/java/com/example/my1stapplication/SettingsActivity.kt
@@ -1,11 +1,32 @@
 package com.example.my1stapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.setting)
+        setContentView(R.layout.setting_activity)
+
+        val nav_arrow = findViewById<ImageView>(R.id.nav_arrow)
+        val log_icon = findViewById<ImageView>(R.id.log_icon)
+
+        // Navigate to MainActivity
+        nav_arrow.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        // Navigate to LoginActivity
+        log_icon.setOnClickListener {
+            val intent = Intent(this, LogActivity::class.java)
+            // Optional: Clear all previous activities
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+            finish()
+        }
     }
 }
